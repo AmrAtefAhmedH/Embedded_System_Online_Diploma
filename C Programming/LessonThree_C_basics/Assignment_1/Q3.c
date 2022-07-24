cf1a028183832e52784a6e4a04377c8df9e23a07
AmrAtefAhmedH <amratef201718@gmail.com>
2022-07-24 08:51:28 +0200

    Write C Program to Add Two Integers

---
+++ C Programming/LessonThree_C_basics/Assignment_1/Q3.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 24, 2022
+ *      Author: Amr Atef
+ */
+#include "stdio.h"
+int main()
+{
+	int x , y ;
+	printf("Enter two integers: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d %d",&x ,&y);
+	printf("Sum: %d",x+y);
+}
