61936dbc18f2e43deffcdd4c7c76d53a3aa32a0a
AmrAtefAhmedH <amratef201718@gmail.com>
2022-07-24 08:54:55 +0200

    Write C Program to Multiply two Floating Point Numbers

---
+++ C Programming/LessonThree_C_basics/Assignment_1/Q4.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 24, 2022
+ *      Author: Amr Atef
+ */
+#include "stdio.h"
+int main()
+{
+	float x , y ;
+	printf("Enter two number: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%f %f",&x ,&y);
+	printf("Product: %f",x*y);
+}
