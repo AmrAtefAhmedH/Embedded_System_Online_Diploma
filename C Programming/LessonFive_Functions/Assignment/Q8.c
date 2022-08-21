625e893136bda08ecaab4f259c04376adbdbc6e0
AmrAtefAhmedH <amratef201718@gmail.com>
2022-08-21 21:12:10 +0200

    modified the C program to sum all digit of number

--- C Programming/LessonFive_Functions/Assignment/Q8.c
+++ C Programming/LessonFive_Functions/Assignment/Q8.c
@@ -6,35 +6,25 @@
  */
 #include "stdio.h"
 #include "string.h"
-void split_String(char String[] , int Array[] , int N);
-int Add_digits(int Array[],int N);
+int Add_digits(char Array[],int N);
 int main()
 {
 	char Number_String[100];
-	int Numbers[100];
 	int N;
 	printf("Enter your number: ");
 	fflush(stdin);fflush(stdout);
 	scanf("%s",Number_String);
 	N = strlen(Number_String);
-	split_String(Number_String,Numbers,N);
-	printf("%d",Add_digits(Numbers,N));
+	printf("%d",Add_digits(Number_String,N));
 	return 0 ;
 }
-/*Function to split the digits into individual number*/
-void split_String(char String[] , int Array[] , int N)
-{
-	int i ;
-	for (i=0 ; i < N; i++)
-		Array[i] = String[i] - '0';
-}
+
 /* function to add the individual numbers in the array and return the sum*/
-int Add_digits(int Array[],int N)
+int Add_digits(char Array[],int N)
 {
 	int i ;
 	int sum = 0 ;
 	for (i = 0 ; i < N ; i++)
-		sum += Array[i];
+		sum += (Array[i]-'0');
 	return sum;
 }
-
