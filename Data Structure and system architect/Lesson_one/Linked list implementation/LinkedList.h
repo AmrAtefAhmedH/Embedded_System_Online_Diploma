9c0f8fbc705e28d50cbb63909d7b49925bda5e40
AmrAtefAhmedH <amratef201718@gmail.com>
2023-08-09 08:14:58 +0300

    The header file of linked list to include it

---
+++ Data Structure and system architect/Lesson_one/Linked list implementation/LinkedList.h
@@ -0,0 +1,40 @@
+/*
+ * LinkedList.h
+ *
+ *  Created on: Aug 8, 2023
+ *      Author: user
+ */
+
+#ifndef LINKEDLIST_H_
+#define LINKEDLIST_H_
+
+struct node {
+	int data;
+	struct node *nextElement;
+};
+
+typedef struct node Node;
+typedef Node* PtrToNode;
+/* ============= Hint =============
+ *
+ * PtrToNode x ----> Node* x ---> struct node* x
+ * pointer to node
+ *
+ * ============= Hint =============
+ *
+ * PtrToNode* x ---> Node** x ---> struct node** x
+ * Pointer to pointer to node
+ *
+ */
+
+/* APIs */
+
+int Linked_isEmpty(PtrToNode );
+void Linked_insertFirst(PtrToNode* ,int );
+void Linked_insertBack(PtrToNode* ,int );
+void Linked_printList(PtrToNode );
+void Linked_Delete(PtrToNode* , int);
+void Linked_ClearList(PtrToNode*);
+void Linked_insertBetween(PtrToNode* ,int , int , int);
+
+#endif /* LINKEDLIST_H_ */
