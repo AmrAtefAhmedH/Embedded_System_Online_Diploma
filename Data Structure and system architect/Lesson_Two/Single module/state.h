60594716e5749dd97910f0a3d18d94f71d6893a8
AmrAtefAhmedH <amratef201718@gmail.com>
2023-08-12 08:33:23 +0300

    prototyping to gerenating functions

---
+++ Data Structure and system architect/Lesson_Two/Single module/state.h
@@ -0,0 +1,16 @@
+/*
+ * state.h
+ *
+ *  Created on: Aug 11, 2023
+ *      Author: user
+ */
+
+#ifndef STATE_H_
+#define STATE_H_
+#include "stdio.h"
+#include "stdlib.h"
+// Automatic state function generated
+#define STATE_Define(state) void State_##state()
+#define STATE(state) State_##state
+
+#endif /* STATE_H_ */
